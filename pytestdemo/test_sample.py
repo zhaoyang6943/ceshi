7cf14698581b12b14f2f8109d8678492471a869f
zhaoyang6943 <z18217276943@163.com>
2021-04-08 23:39:46 +0800

    第一次提交

---
+++ pytestdemo/test_sample.py
@@ -0,0 +1,12 @@
+import pytest
+# content of test_sample.py
+def inc(x):
+    return x + 1
+
+
+def test_answer():
+    assert inc(3) == 5
+
+@pytest.mark.login
+def test_answer1():
+    assert inc(3) == 5
